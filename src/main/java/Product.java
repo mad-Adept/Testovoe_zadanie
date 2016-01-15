2ad456edbe5ad86d23ca285b0de01e2f71b82d3b
mad-Adept <Antoxa-Nikonov@ya.ru>
2016-01-15 16:32:22 +0300

    На работе: инициализация Product и Works

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,11 +1,44 @@
+import java.util.ArrayList;
 
 public class Product {
-    int id;
     String name;
     int weight;
-    int Price;
+    int price;
+    ArrayList<String> name_products = new ArrayList();
 
     public Product() {
     }
 
+    public Product(String name, int weight, int price) {
+        if (check_name(name)){
+            System.out.println("Product name already exists!");
+        }
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+        name_products.add(name);
+    }
+
+    public boolean check_name (String name){
+        for (String iterator : name_products){
+            if (iterator.equals(name)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return name.equals(product.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -7,4 +7,33 @@ public class Worker {
     ArrayList<Product> menu = new ArrayList();
     public Worker() {
     }
+    public Worker(String f_name,String s_name, Product pruduct_name){
+        this.firstName = f_name;
+        this.secondName = s_name;
+        menu.add(pruduct_name);
+        id++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Worker worker = (Worker) o;
+
+        if (id != worker.id) return false;
+        if (!firstName.equals(worker.firstName)) return false;
+        if (!secondName.equals(worker.secondName)) return false;
+        return !(menu != null ? !menu.equals(worker.menu) : worker.menu != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + secondName.hashCode();
+        result = 31 * result + (menu != null ? menu.hashCode() : 0);
+        return result;
+    }
 }
